d7f49a3c96071d805977c486e6b52d7344ba2489
Himani <himanikinkar02@gmail.com>
2022-03-27 04:50:48 +0530

    nnn

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import Topbar from "../../components/topbar/topbar";
 import Sidebar from "../../components/sidebar/sidebar";
 import Feed from "../../components/feed/feed";
 import Rightbar from "../../components/rightbar/rightbar";
+// import Post from "../../components/posts/Post";
 import "./home.css";
 
 export default function Home() {
